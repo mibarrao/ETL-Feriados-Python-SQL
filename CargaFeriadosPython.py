e031e2602a5bd8478be1a97c6004672ead7e4a2d
María Ester Ibarra Orellana <58990011+mibarrao@users.noreply.github.com>
2026-03-26 11:21:56 -0300

    Carga inicial del script ETL

---
+++ CargaFeriadosPython.py
@@ -0,0 +1,102 @@
+﻿import requests
+import pyodbc
+from datetime import datetime
+
+def etl_feriados_calendario():
+    # ==========================================
+    # 1. CONFIGURACIÓN DE VARIABLES
+    # ==========================================
+    url_api = "https://api.boostr.cl/holidays/2025.json"
+    anio_proceso = 2025    
+    SERVIDOR = r'NBK-AV-MIBARRA\SQLEXPRESS'
+    BASE_DE_DATOS = 'ESTUDIOSCOMERCIALES' 
+    
+    # Cadena de conexión usando Autenticación de Windows
+    conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER=LH-GESTIONDOS2;DATABASE=ESTUDIOSCOMERCIALES;Trusted_Connection=yes;'
+    
+    
+    try:
+        # ==========================================
+        # 2. EXTRACCIÓN (Extraer datos de la API)
+        # ==========================================
+        print(f"1. Conectando a la API para el año {anio_proceso}...")
+        respuesta = requests.get(url_api)
+        respuesta.raise_for_status() # Si la página se encuentra caida 404 o 500 detiene el código
+        datos_json = respuesta.json()
+        
+        if datos_json.get('status') != 'success':
+            print("Error: La API no devolvió un estado de éxito.")
+            return
+
+        lista_feriados = datos_json.get('data', [])
+        print(f"   -> ¡Éxito! Se encontraron {len(lista_feriados)} feriados.\n")
+
+        # ==========================================
+        # 3. CARGA (Insertar en SQL Server)
+        # ==========================================
+        print("2. Conectando a SQL Server...")
+        conexion = pyodbc.connect(conn_str)
+        cursor = conexion.cursor()
+        print("   -> Conexión establecida.\n")
+
+        print("3. Limpiando la tabla dbo.feriado (Truncate)...")
+        cursor.execute("TRUNCATE TABLE dbo.feriado")
+        
+        print("4. Insertando nuevos registros...")
+        #id_counter = 1
+        
+        # Consulta SQL preparada
+        query_insert = """
+            INSERT INTO dbo.feriado ( Fecha, Detalle, Irrenunciable, Activo)
+            VALUES ( ?, ?, ?, ?)
+        """
+        
+        for feriado in lista_feriados:
+            fecha_str = feriado.get('date')
+            titulo = feriado.get('title')
+            
+            # Truncamos a 60 caracteres por si acaso, tal como en tu tabla
+            if titulo and len(titulo) > 60:
+                titulo = titulo[:60]
+                
+            irrenunciable = 1 if feriado.get('inalienable') else 0
+            activo = 1
+            
+            # Ejecutamos el insert fila por fila
+            cursor.execute(query_insert, ( fecha_str, titulo, irrenunciable, activo))
+           #id_counter += 1
+            
+        #print(f"   -> Se insertaron {id_counter - 1} feriados correctamente.\n")
+
+        conexion.commit() # Guarda insert generados
+
+        # ==========================================
+        # 4. EJECUCIÓN DE PROCEDIMIENTO ALMACENADO
+        # ==========================================
+        print(f"5. Ejecutando SP usp_GenerarCalendario para el año {anio_proceso}...")
+        query_sp = "EXEC dbo.usp_ETLCargaCalendarioAnual"
+        cursor.execute(query_sp)
+        print("   -> Procedimiento ejecutado con éxito.\n")
+
+
+        
+        # ==========================================
+        # 5. CONFIRMAR CAMBIOS (COMMIT)
+        # ==========================================
+        conexion.commit()
+        print("=== PROCESO ETL FINALIZADO CORRECTAMENTE ===")
+
+    except pyodbc.Error as e:
+        print(f"\n❌ Error de Base de Datos: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"\n❌ Error de API: {e}")
+    except Exception as e:
+        print(f"\n❌ Error General: {e}")
+    finally:
+        # Siempre cerramos la conexión pase lo que pase
+        if 'conexion' in locals():
+            conexion.close()
+
+# Punto de entrada
+if __name__ == "__main__":
+    etl_feriados_calendario()
---
+++ usp_ETLCargaCalendarioAnual.sql
@@ -0,0 +1,96 @@
+
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<María Ibarra Orellana>
+-- Create date: <25-03-2026>
+-- Description:	<Generación automática para Calendario anual>
+-- =============================================
+ALTER PROCEDURE usp_ETLCargaCalendarioAnual
+	
+AS
+BEGIN
+
+
+DROP TABLE IF EXISTS dbo.calendario;
+
+CREATE TABLE dbo.calendario (
+    Fecha DATE PRIMARY KEY,
+    DiaHabilDelMes INT,
+	NumeroSemana INT, -- ¡Nueva columna agregada!
+    NombreDia VARCHAR(20),
+    
+);
+
+/***/
+
+
+		-- 1. Configuramos el primer día de la semana como Lunes (1)
+		SET DATEFIRST 1; 
+
+		-- 2. Definimos el año que queremos generar
+		DECLARE @Anio INT = 2025; -- Cambia esto al año que necesites
+		DECLARE @FechaInicio DATE = DATEFROMPARTS(@Anio, 1, 1);
+		DECLARE @FechaFin DATE = DATEFROMPARTS(@Anio, 12, 31);
+
+		-- 3. CTE Recursivo para generar todas las fechas del año
+		WITH GeneradorFechas AS (
+			SELECT @FechaInicio AS Fecha
+			UNION ALL
+			SELECT DATEADD(DAY, 1, Fecha)
+			FROM GeneradorFechas
+			WHERE Fecha < @FechaFin
+		),
+		-- 4. CTE para cruzar con feriados y determinar qué tipo de día es
+
+
+		ClasificadorDias AS (
+			SELECT 
+				gf.Fecha,
+				YEAR(gf.Fecha) AS Anio,
+				MONTH(gf.Fecha) AS Mes,
+				DAY(gf.Fecha) AS Dia,
+        
+				-- Calculamos el número de semana bajo el estándar ISO
+				DATEPART(ISO_WEEK, gf.Fecha) AS NumeroSemana,
+        
+				DATENAME(WEEKDAY, gf.Fecha) AS NombreDia,
+        
+				-- Si es Sábado (6) o Domingo (7), es fin de semana
+				--CASE WHEN DATEPART(WEEKDAY, gf.Fecha) IN (6, 7) THEN 1 ELSE 0 END AS EsFinDeSemana,
+				
+				-- Si cruzó con la tabla feriados, es feriado
+				CASE WHEN fer.Fecha IS NOT NULL THEN 1 ELSE 0 END AS EsFeriado,
+        
+				-- Es hábil SOLO si no es fin de semana y no es feriado
+				CASE 
+					WHEN DATEPART(WEEKDAY, gf.Fecha) IN (6, 7) THEN 0 
+					WHEN fer.Fecha IS NOT NULL THEN 0 
+					ELSE 1 
+				END AS EsHabil
+			FROM GeneradorFechas gf
+			LEFT JOIN dbo.feriado fer 
+				ON gf.Fecha = fer.Fecha AND fer.Activo = 1
+		)
+		-- 5. Insertamos el resultado final
+		INSERT INTO dbo.calendario (Fecha, DiaHabilDelMes , NumeroSemana, NombreDia)
+		SELECT 
+			Fecha,
+			 -- Calculamos el correlativo del día hábil
+			CASE 
+				WHEN EsHabil = 1 THEN 
+					ROW_NUMBER() OVER (PARTITION BY Anio, Mes, EsHabil ORDER BY Fecha)
+				ELSE 0 
+			END AS DiaHabilDelMes,
+			NumeroSemana,
+			NombreDia
+		FROM ClasificadorDias
+		order by fecha 
+		OPTION (MAXRECURSION 366);
+
+
+END
+GO
